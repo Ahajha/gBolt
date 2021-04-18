1d009f885e95e23a7fbfa036319a3e9b420cc47c
Alex <ahajha@gmail.com>
2021-04-18 17:10:13 -0400

    add simple benchmark and clang-tidy scripts

---
+++ scripts/simple-bench.sh
@@ -0,0 +1,1 @@
+/usr/bin/time -f "%E\n%M kb max" ./build/gbolt -i ./extern/data/Compound_422 -s 0.06
---
+++ scripts/tidy.sh
@@ -0,0 +1,1 @@
+clang-tidy src/* --checks=modernize-*,-modernize-use-trailing-return-type -- -I include/ -I extern/cxxopts/include/
