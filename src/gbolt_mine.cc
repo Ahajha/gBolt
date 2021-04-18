5d0c889543d832dde57d889755f63c35194ffbb9
Alex <ahajha@gmail.com>
2021-04-18 05:04:17 -0400

    enhance some for loops in gbot_mine.cc

--- src/gbolt_mine.cc
+++ src/gbolt_mine.cc
@@ -12,29 +12,27 @@ void GBolt::find_frequent_nodes_and_edges(const vector<Graph> &graphs) {
   for (auto i = 0; i < graphs.size(); ++i) {
     unordered_set<int> vertex_set;
     unordered_set<int> edge_set;
-    for (auto j = 0; j < graphs[i].size(); ++j) {
-      const vertex_t *vertex = graphs[i].get_p_vertex(j);
-      vertex_set.insert(vertex->label);
-      for (auto k = 0; k < (vertex->edges).size(); ++k) {
-        edge_set.insert(vertex->edges[k].label);
+    for (const auto& vertex : *(graphs[i].get_p_vertice())) {
+      vertex_set.insert(vertex.label);
+      for (const auto& edge : vertex.edges) {
+        edge_set.insert(edge.label);
       }
     }
-    for (auto it = vertex_set.begin(); it != vertex_set.end(); ++it) {
-      vertex_labels[*it].emplace_back(i);
+    for (auto label : vertex_set) {
+      vertex_labels[label].emplace_back(i);
     }
-    for (auto it = edge_set.begin(); it != edge_set.end(); ++it) {
-      ++edge_labels[*it];
-    }
-  }
-  for (auto it = vertex_labels.begin(); it != vertex_labels.end(); ++it) {
-    if (it->second.size() >= nsupport_) {
-      frequent_vertex_labels_.insert(std::make_pair(it->first, it->second));
+    for (auto label : edge_set) {
+      ++edge_labels[label];
     }
   }
-  for (auto it = edge_labels.begin();
-    it != edge_labels.end(); ++it) {
-    if (it->second >= nsupport_) {
-      frequent_edge_labels_.insert(std::make_pair(it->first, it->second));
+  for (const auto& kv_pair : vertex_labels) {
+    if (kv_pair.second.size() >= nsupport_) {
+      frequent_vertex_labels_.insert(kv_pair);
+    }
+  }
+  for (const auto& kv_pair : edge_labels) {
+    if (kv_pair.second >= nsupport_) {
+      frequent_edge_labels_.insert(kv_pair);
     }
   }
 }
@@ -45,17 +43,16 @@ void GBolt::report(const DfsCodes &dfs_codes, const Projection &projection,
   Graph graph;
   build_graph(dfs_codes, graph);
 
-  for (auto i = 0; i < graph.size(); ++i) {
-    const vertex_t *vertex = graph.get_p_vertex(i);
-    ss << "v " << vertex->id << " " << vertex->label << std::endl;
+  for (const auto& vertex : *(graph.get_p_vertice())) {
+    ss << "v " << vertex.id << " " << vertex.label << std::endl;
   }
-  for (auto i = 0; i < dfs_codes.size(); ++i) {
-    ss << "e " << dfs_codes[i]->from << " " << dfs_codes[i]->to
-      << " " << dfs_codes[i]->edge_label << std::endl;
+  for (const auto edge : dfs_codes) {
+    ss << "e " << edge->from << " " << edge->to
+      << " " << edge->edge_label << std::endl;
   }
   ss << "x: ";
   int prev = 0;
-  for (auto i = 0; i < projection.size(); ++i) {
+  for (std::size_t i = 0; i < projection.size(); ++i) {
     if (i == 0 || projection[i].id != prev) {
       prev = projection[i].id;
       ss << prev << " ";
@@ -89,19 +86,18 @@ void GBolt::save(bool output_parent, bool output_pattern, bool output_frequent_n
     output_frequent_nodes_ = new Output(output_file_nodes);
 
     int graph_id = 0;
-    for (auto it = frequent_vertex_labels_.begin();
-      it != frequent_vertex_labels_.end(); ++it) {
+    for (const auto kv_pair : frequent_vertex_labels_) {
       std::stringstream ss;
 
-      ss << "v 0 " + std::to_string(it->first);
+      ss << "v 0 " + std::to_string(kv_pair.first);
       ss << std::endl;
       ss << "x: ";
-      for (auto i = 0; i < it->second.size(); ++i) {
-        ss << it->second[i] << " ";
+      for (auto g_id : kv_pair.second) {
+        ss << g_id << " ";
       }
       ss << std::endl;
 
-      output_frequent_nodes_->push_back(ss.str(), it->second.size(), graph_id++);
+      output_frequent_nodes_->push_back(ss.str(), kv_pair.second.size(), graph_id++);
     }
     output_frequent_nodes_->save(false, true);
   }
